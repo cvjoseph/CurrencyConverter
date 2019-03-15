dd7ff44bf55f160ee7429cba8f25659dfb360505
Joseph Verghese <cvjoseph@gmail.com>
2019-03-15 12:43:02 +0400

    Updated fx source

---
+++ Rate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    public class Rate
+    {
+        public string to { get; set; }
+        public string from { get; set; }
+        public double rate { get; set; }
+    }
+}
